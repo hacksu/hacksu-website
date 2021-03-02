8e5bf6f111f162c1f59ccc3191a45fd5cef33949
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-03-02 18:11:49 -0500

    social media

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="footer">
-    <img src="../assets/facebook-white.svg">
-    <img src="../assets/twitter-white.svg">
-    <img src="../assets/instagram-white.svg">
-    <img src="../assets/github-white.svg">
+    <a class="social" href="https://www.facebook.com/groups/hacksu"><img src="../assets/facebook-white.svg"></a>
+    <a class="social" href="https://twitter.com/_hacksu"><img src="../assets/twitter-white.svg"></a>
+    <a class="social" href="https://www.instagram.com/hacksu/"><img src="../assets/instagram-white.svg"></a>
+    <a class="social" href="https://github.com/hacksu"><img src="../assets/github-padded-white.svg"></a>
+    <a v-if="discord" class="social" v-bind:href="discord"><img src="../assets/discord-square-white.svg"></a>
   </div>
 </template>
+
+<script>
+import { discord } from '@/details';
+
+export default {
+  name: 'SocialMedia',
+  data() {
+    return {
+      discord,
+    }
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 
 .footer {
   padding-bottom: 5vh;
   background-color: rgb(20, 32, 39);
+  text-align: center;
+  .social {
+    display: inline-block;
+    padding: 5vh 1.25vh;
+    img {
+      height: 10vh;
+      max-height: 15vw;
+    }
+  }
 }
 
 </style>
