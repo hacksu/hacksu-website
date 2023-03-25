f6b45bb5cbaffea84175bb83ab10fc24c51d4321
toBeOfUse <mitchjacov@gmail.com>
2023-03-25 02:08:35 -0400

    remove unused server code

--- server/index.js
+++ server/index.js
@@ -1,52 +1,26 @@
-global.config = require(__dirname + '/../server.config.json');
+global.config = require(__dirname + "/../server.config.json");
 
-let express = require('express');
-let fetch = require('node-fetch');
-
-global.LOCALHOST = require('os').hostname() !== 'hacksu-com';
-
-require('./discord.temp');
+let express = require("express");
 
 let app = express();
-app.set('trust proxy', 1);
-
-let session = require('express-session');
-let store = new (require('connect-mongodb-session')(session))({
-    uri: 'mongodb://localhost:27017/hacksu-website',
-    collection: 'sessions',
-});
-app.use(session({
-    secret: config.session.secret,
-    saveUninitialized: false,
-    resave: false,
-    store,
-}));
-
+app.set("trust proxy", 1);
 
 // mount to / on port 8000 for NGINX reverse proxy
-app.use(require('./routes'));
-app.use('/api', (req, res, next) => {
-    req.headers['x-forwarded-for'] = (
-        req.headers['x-forwarded-for'] || req.headers['x-real-ip']
-    );
-    req.ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+app.use(require("./routes"));
+app.use(
+  "/api",
+  (req, res, next) => {
+    req.headers["x-forwarded-for"] =
+      req.headers["x-forwarded-for"] || req.headers["x-real-ip"];
+    req.ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     next();
-}, require('./routes/api'));
+  },
+  require("./routes/api")
+);
 
-app.use(require('./redirects'));
-
-// if (process.env.PORT) {
-    let dist = `${__dirname}/../dist`;
-    let index = require('path').resolve(`${dist}/index.html`);
-    let serve = express.static(dist);
-    app.use(serve, (req, res, next) => {
-        res.sendFile(index);
-    });
-// } else {
-//     app.use(require('express-http-proxy')(`http://localhost:8080`));
-// }
+app.use(require("./redirects"));
 
 let port = process.env.PORT || 8000;
 app.listen(port, function () {
-    console.log('Listening on port', port);
-})
+  console.log("Listening on port", port);
+});
--- server/redirects.js
+++ server/redirects.js
@@ -1,32 +1,41 @@
-let express = require('express');
-let fetch = require('node-fetch');
+let express = require("express");
+let fetch = require("node-fetch");
 let app = express.Router();
 module.exports = app;
 
 // Redirects
-let redirects = false;
+let redirects = {};
 let lastFetchedRedirects = 0;
-let redirectsCacheDuration = (10 * 60 * 1000);
+let redirectsCacheDuration = 10 * 60 * 1000;
 let fetchRedirects = async function () {
-    if (Date.now() - lastFetchedRedirects >= redirectsCacheDuration) {
-        lastFetchedRedirects = Date.now();
-        let body = await (await fetch('https://raw.githubusercontent.com/wiki/hacksu/hacksu-2021/Hacksu-Website-Redirects.md')).text();
-        redirects = Object.fromEntries(body.split('\n').filter(o => o[0] == '/').map(o => o.trim().split(' ')));
-    }
+  if (Date.now() - lastFetchedRedirects >= redirectsCacheDuration) {
+    lastFetchedRedirects = Date.now();
+    let body = await (
+      await fetch(
+        "https://raw.githubusercontent.com/wiki/hacksu/hacksu-2021/Hacksu-Website-Redirects.md"
+      )
+    ).text();
+    redirects = Object.fromEntries(
+      body
+        .split("\n")
+        .filter((o) => o[0] == "/")
+        .map((o) => o.trim().split(" "))
+    );
+  }
 };
 
-app.get('/refresh-redirects', (req, res) => {
-    lastFetchedRedirects = 0;
-    fetchRedirects();
-    res.send(JSON.stringify(redirects));
+app.get("/refresh-redirects", (req, res) => {
+  lastFetchedRedirects = 0;
+  fetchRedirects();
+  res.send(JSON.stringify(redirects));
 });
 
 app.use(async (req, res, next) => {
-    await fetchRedirects();
-    if (req.path in redirects) {
-        res.redirect(redirects[req.path]);
-    } else {
-        next();
-    }
+  await fetchRedirects();
+  if (req.path in redirects) {
+    res.redirect(redirects[req.path]);
+  } else {
+    next();
+  }
 });
 fetchRedirects();
