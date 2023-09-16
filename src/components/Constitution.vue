2cabd790f8e94569a3a4b6b4b609c6d065ab2d4d
Brandon Scholten <scholtenb8890@mtc.edu>
2023-09-15 22:15:51 -0400

    switched back to PDF embed

---
+++ src/components/Constitution.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="constitution">
+    <embed id="embed" type="application/pdf" width="100%" src="constitution.pdf">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Constitution',
+}
+</script>
+
+<style scoped lang="scss">
+.constitution {
+  max-height: 100vh;
+  overflow-y: hidden;
+  background-color: #142027;
+  embed {
+    height: $navbar-height-remaining;
+    margin-top: $navbar-height;
+  }
+}
+</style>
--- src/views/Constitution.vue
+++ src/views/Constitution.vue
@@ -1,32 +1,6 @@
-<template>
-  <!-- <div class="constitution">
-    <embed data-v id="embed" src="constitution.pdf" type="application/pdf" width="100%">
-  </div> -->
-  <!-- <div class="constitution">
-   <pdf src="constitution.pdf"></pdf>
-  </div> -->
-  <div class="constitution">
-    <vue-pdf-embed class="embed" source="constitution.pdf" />
-  </div>
-</template>
-
 <script setup>
-
-import VuePdfEmbed from 'vue-pdf-embed'
-
-// // @ is an alias to /src
-
-// export default {
-//   name: 'Constitution',
-
-// }
+import Constitution from '../components/Constitution.vue';
 </script>
-
-<style scoped lang="scss">
-
-.constitution {
-  max-width: 1000px;
-}
-
-
-</style>
+<template>
+        <Constitution />
+</template>
