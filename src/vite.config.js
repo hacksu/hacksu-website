5f4167f436c6aa5ae6942bf5600d82552111efe8
Struck713 <noahstruck71303@gmail.com>
2025-06-18 23:00:42 -0400

    Add Dockerfile and docker-compose.yml for ease of use with Bun

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import path from 'path';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,8 +27,7 @@ export default defineConfig({
     }
   },
   build: {
-    // files are moved to dist by github actions cd
-    outDir: "../dist2"
+    outDir: "../dist"
   },
   server: {
     watch: {
