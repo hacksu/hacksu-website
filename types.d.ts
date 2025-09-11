cb0721ccfdd409c198eb6f83a5bcb92dfa78eb4e
Struck713 <noahstruck71303@gmail.com>
2025-09-10 22:10:07 -0400

    Change all Remult entities to be decorated

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,18 @@
-declare namespace Express {
-    export interface User {
-        id: string;
-        username?: string;
+declare global {
+    namespace Express {
+        interface User {
+            id: string;
+            username?: string;
+            isLeader?: boolean;
+        }
+    }
+}
+
+declare module 'remult' {
+    interface UserInfo {
         isLeader?: boolean;
     }
 }
+
+export { };
+
